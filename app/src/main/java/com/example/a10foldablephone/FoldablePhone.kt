e6e3a292c2e3e6e7944faa7133e87d3c110ea490
CAT <cavilest@gmail.com>
2023-07-12 21:55:00 -0400

    Ejercicio individual 10 - Creación de clase FoldablePhone. - Implementación de métodos.

--- app/src/main/java/com/example/a10foldablephone/FoldablePhone.kt
+++ app/src/main/java/com/example/a10foldablephone/FoldablePhone.kt
@@ -1,20 +1,25 @@
 package com.example.a10foldablephone
 
+// Escribe una clase FoldablePhone que se herede de la clase Phone.
 class FoldablePhone(isScreenLightOn: Boolean = false) : Phone(isScreenLightOn) {
 
+    // Una propiedad que indique si el teléfono está plegado.
     var isFolded: Boolean = false
 
+    // Un comportamiento de función switchOn() diferente del de la clase Phone para que
+    // solo encienda la pantalla cuando el teléfono no esté plegado.
     override fun switchOn() {
         if (!isFolded) {
             isScreenLightOn = true
         }
     }
 
-    fun abrir() {
+    // Métodos para cambiar el estado de plegado.
+    fun isClosed() {
         isFolded = true
     }
 
-    fun cerrar() {
+    fun isOpen() {
         isFolded = false
     }
 }
